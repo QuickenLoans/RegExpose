76968225f8c3e0097088237f0642ef13b1197a53
Brian Friesen <brian.friesen@gmail.com>
2013-01-14 03:16:01 -0500

    Added Service Stack to Web project.

--- RegExpose.Web/App_Start/RouteConfig.cs
+++ RegExpose.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RegExpose.Web
@@ -11,6 +7,9 @@ namespace RegExpose.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("api/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" }); //Prevent exceptions for favicon
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
