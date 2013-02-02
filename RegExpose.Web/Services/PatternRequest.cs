a1393a3210beea680e945d2a61f44536f531009e
Brian Friesen <brian.friesen@gmail.com>
2013-02-02 15:34:11 -0500

    Removed ss nuget defaults; added pattern service.

---
+++ RegExpose.Web/Services/PatternRequest.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegExpose.Web.Services
+{
+    public class PatternRequest : IReturn<PatternResponse>
+    {
+        public string Pattern { get; set; }
+        public bool IgnoreCase { get; set; }
+        public bool SingleLine { get; set; }
+        public bool MultiLine { get; set; }
+    }
+}
---
+++ RegExpose.Web/Services/RegexNodeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegExpose.Web.Services
+{
+    public class RegexNodeDto
+    {
+        public RegexNodeDto()
+        {
+            Children = new List<RegexNodeDto>();
+        }
+
+        public int Id { get; set; }
+        public int Index { get; set; }
+        public string Pattern { get; set; }
+        public string NodeType { get; set; }
+        public List<RegexNodeDto> Children { get; set; }
+    }
+}
