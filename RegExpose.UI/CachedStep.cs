66a9a5a1ee648db235cc7bf59a990e7d776835e0
Brian Friesen <brianfriesen@quickenloans.com>
2013-07-09 03:08:26 -0400

    Added 'regex index' arrow to WinForms UI. Always shows the initial index where the regex starts an attempt at parsing. Only advances upon success of failure of the entire regex.

--- RegExpose.UI/CachedStep.cs
+++ RegExpose.UI/CachedStep.cs
@@ -4,10 +4,11 @@ namespace RegExpose.UI
 {
     public class CachedStep
     {
-        public CachedStep(ParseStep step, int stepIndex, int currentIndex, int currentLookaroundIndex, IEnumerable<int> savedStatesIndexes)
+        public CachedStep(ParseStep step, int stepIndex, int regexIndex, int currentIndex, int currentLookaroundIndex, IEnumerable<int> savedStatesIndexes)
         {
             Step = step;
             StepIndex = stepIndex;
+            RegexIndex = regexIndex;
             CurrentIndex = currentIndex;
             CurrentLookaroundIndex = currentLookaroundIndex;
             SavedStatesIndexes = savedStatesIndexes;
@@ -15,6 +16,7 @@ namespace RegExpose.UI
 
         public ParseStep Step { get; private set; }
         public int StepIndex { get; private set; }
+        public int RegexIndex { get; private set; }
         public int CurrentIndex { get; private set; }
         public IEnumerable<int> SavedStatesIndexes { get; private set; }
         public int CurrentLookaroundIndex { get; private set; }
